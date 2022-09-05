6b8b3d89184f30c3e03db9b4ffe2ef14fbd1c2f4
Eneias <medinasp@gmail.com>
2022-09-04 22:57:33 -0300

    Primeiro Controller e testes nas paginas Razor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
